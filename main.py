22fc2c0780e8226f7089d1fa401399e2b09c2c20
Nicholas Gibb <nicholas.gibb@canada.ca>
2020-10-15 17:42:01 +0000

    Refactor and update shapefile for HRs

--- main.py
+++ main.py
@@ -1,44 +1,65 @@
+from datetime import datetime
 import json
-
+import os
 import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
 
-places_df = pd.read_csv('data/ECCCdata_19_0.csv')
-places_df['coords'] = list(zip(places_df.LONGITUDE, places_df.LATITUDE))
 
-points_dict = gpd.GeoDataFrame(
-    places_df, crs={'init': 'epsg:4269'},
-    geometry=places_df['coords'].apply(Point)
-).to_dict('records')
+def load_data():
+    places_df = pd.read_csv(os.path.join('data', 'ECCCdata_19_0.csv'))
+    places_df['coords'] = list(zip(places_df.LONGITUDE, places_df.LATITUDE))
 
-# mun_gdf = gdp.read_file('https://opendata.arcgis.com/datasets/3aa9f7b1428642998fa399c57dad8045_0.geojson')
-polys_dict = gpd.read_file(
-    'data/Health_Regional_Archive__Public_View_.geojson').to_dict('records')
-poly_geoms = [poly['geometry'] for poly in polys_dict]
+    points_dict = gpd.GeoDataFrame(
+        places_df, crs='epsg:4269',
+        geometry=places_df['coords'].apply(Point)
+    ).to_dict('records')
+
+    HR_PATH = os.path.join('data', 'HR COVID',
+                           'PopEstimates2019_RegionalHealthBoundaries_Lambert.shp')
+    polys_gdf = gpd.read_file(HR_PATH).to_crs(epsg=4269)
+    polys_dict = polys_gdf.to_dict('records')
+
+    return (points_dict, polys_dict)
 
 
-def find_intersecting_poly(pt_geom):
+def find_intersecting_poly(pt_geom, polys_dict):
     for poly in polys_dict:
         if pt_geom.within(poly['geometry']):
             return poly['HR_UID']
     return False
 
 
-for i, point in enumerate(points_dict):
-    if i % 10 == 0:
-        print(f'Processing point {i}')
+def add_HR_UID_field(points_dict, polys_dict):
+    poly_geoms = [poly['geometry'] for poly in polys_dict]
+    for i, point in enumerate(points_dict):
+        if i % 100 == 0:
+            print(f'Processing point {i}')
 
-    HR_UID = find_intersecting_poly(point['geometry'])
+        HR_UID = find_intersecting_poly(point['geometry'], polys_dict)
 
-    if not HR_UID:
+        if not HR_UID:
+            geom_min_poly = min(poly_geoms, key=point['geometry'].distance)
+            closest_HR = [
+                poly for poly in polys_dict if poly['geometry'] == geom_min_poly]
+            HR_UID = closest_HR[0]['HR_UID']
 
-        min_poly = min(poly_geoms, key=point['geometry'].distance)
+        point['HR_UID'] = HR_UID
+        point.pop('geometry')
+    return points_dict
 
-        HR_UID = [poly for poly in polys_dict if poly['geometry']
-                  == min_poly][0]['HR_UID']
 
-    point['HR_UID'] = HR_UID
-    point.pop('geometry')
+def save_csv(points_dict):
+    current_date = datetime.now().date().strftime("%Y-%m-%d")
+    filename = f'places_to_HRs_{current_date}.csv'
+    pd.DataFrame(points_dict).to_csv(filename, index=False)
 
-pd.DataFrame(points_dict).to_csv('output.csv', index=False)
+
+def main():
+    points_dict, polys_dict = load_data()
+    points_dict = add_HR_UID_field(points_dict, polys_dict)
+    save_csv(points_dict)
+
+
+if __name__ == "__main__":
+    main()
